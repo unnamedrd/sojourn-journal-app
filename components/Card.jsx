981fea6ae57705d2cb730652bef65713ab3aef9b
najwanan <theunnamedrd@gmail.com>
2022-09-18 21:20:54 -0700

    mapping data to journal card component

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,5 +4,15 @@ import data from "../public/data";
 
 
 export default function Card(props) {
-    return <div className="cardJournal"></div>;
+    return (
+        <div className="cardJournal">
+            <img src={`../public/images/${props.img}`} className='card-image' />
+            <div className="cardDetails">
+                <p>{props.title}</p>
+                <span>{props.location}</span>
+                <span>{props.description}</span>
+            </div>
+           
+        </div>
+    )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from '../components/NavBar'
 import Card from '../components/Card'
+import data from '../public/data'
 
 function App() {
- 
+  const cards = data.map(item => {
+    return (
+      <Card
+  
+        location={item.location}
+        title={item.title}
+        date={item.start_date, item.end_date}
+        description = {item.description}
+        
+      />
+   )
+  })
+  
+
   return (
     <div className="App">
-      <div>
-       
-      </div>
       <h2>My Trips</h2>
-      
-    
     </div>
   )
 }
